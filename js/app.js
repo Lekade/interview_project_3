d103fb71e00e06584b8313434a3ecfaf99670f04
Chkip <kasperovich.deonis@gmail.com>
2023-05-22 19:34:50 +0300

    pc version

---
+++ js/app.js
@@ -0,0 +1,46 @@
+const  formSelect = document.querySelector(".form__select")
+const  selectTitle = document.querySelector(".select-title")
+const  selectTitleText = document.querySelector(".select-title__text")
+const  selectOption = document.querySelectorAll(".select-option")
+
+
+selectTitle.addEventListener('click', () => {
+    if(!formSelect.classList.contains("active")){
+        return  formSelect.classList.add("active")
+    } else{
+        return  formSelect.classList.remove("active")
+    }
+})
+
+document.addEventListener('click', e => {
+    let click = e.composedPath().includes(formSelect)
+    if(!click){
+        return  formSelect.classList.remove("active")
+    }
+})
+
+const removeOptionActive = () => selectOption.forEach( el => {
+    el.classList.remove("active-option")
+})
+
+selectOption.forEach(el => {
+    el.addEventListener('click', function(){
+        removeOptionActive()
+        el.classList.add("active-option")
+        selectTitleText.innerHTML = el.querySelector("input").value
+        formSelect.classList.remove("active")
+    })
+})
+
+
+
+/*--------------------------------------Range--------------------------------------------*/
+
+const formRangePercent = document.querySelector(".form__range-perсent")
+
+const addValueRange = (e) => {
+    formRangePercent.innerHTML = e
+}
+
+
+
